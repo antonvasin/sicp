dc408871fb11d480cc36bd9e5fa6fd2e68645eaf
Anton Vasin <antonvasin@me.com>
2018-11-03 17:11:54 +0300

    1.2 and 1.3

---
+++ 1.scm
@@ -0,0 +1,27 @@
+; Ex 1.2
+
+(/ (+ 5
+      4
+      (- 2
+        (- 3
+          (+ 6
+            (/ 1 3)))))
+
+    (* 3
+      (- 6 2)
+      (- 2 7)))
+
+; Ex 1.3
+
+(define (sqrt a)
+  (* a a))
+
+(define (sum-sqrt a b)
+  (+ (sqrt a) (sqrt b)))
+
+(define (sum-sqrt-largest a b c)
+  (cond ((and (> a c) (> b c)) (sum-sqrt a b))
+        ((and (> b a) (> c a)) (sum-sqrt b c))
+        ((and (> a b) (> c b)) (sum-sqrt a c))))
+
+(sum-sqrt-largest 2 4 5)
