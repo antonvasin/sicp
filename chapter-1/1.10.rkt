07588ee3ca985eb596e82c64ac30353a92d1decc
Anton Vasin <antonvasin@me.com>
2019-05-28 10:47:59 +0300

    1.10

---
+++ chapter-1/1.10.rkt
@@ -0,0 +1,26 @@
+#lang sicp
+(define (A x y)
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (- x 1) (A x (- y 1))))))
+
+(A 1 10)
+(A 2 4)
+(A 3 3)
+
+; 2 * n
+(define (f n) (A 0 n))
+(f 4)
+
+; 2 ** n
+(define (g n) (A 1 n))
+(g 4)
+
+; 2 ** (n * 2)
+(define (h n) (A 2 n))
+(h 4)
+
+; (5 * n) ** 2
+(define (k n) (* 5 n n))
+(n 2)
