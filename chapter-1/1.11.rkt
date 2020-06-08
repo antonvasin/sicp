703e886eb8cf5a0b686ca4a20ec3e979f055bb4d
Anton Vasin <antonvasin@me.com>
2020-06-08 21:47:01 +0300

    finished 1.11

--- chapter-1/1.11.rkt
+++ chapter-1/1.11.rkt
@@ -1,15 +1,50 @@
 #lang sicp
 
 (define (f-rec n)
+  #| (display "n: ") (display n) (display "\n") |#
   (cond ((< n 3) n)
-        ((>= n 3) (+ (f (- n 1)) (* 2 (f (- n 2))) (* 3 (f (- n 3))) ))
+        (else (+
+                (f-rec (- n 1))
+                (* 2 (f-rec (- n 2)))
+                (* 3 (f-rec (- n 3)))))
         ))
 
 (f-rec 2)
 (f-rec 3)
 (f-rec 4)
 
-; (define (f-iter n)
-;   (cond ((< n 3) n)
-;         ((>= n 3) )
-;         ))
+#|
+(f 4)
+(+ (f 3) (* 2 (f 2)) (* 3 (f 1)))
+(+ (+ (f 2) (* 2 (f 1)) (* 3 (f 0))) (* 2 2) (* 3 1))
+(+ (+ 2 (* 2 1) (* 3 0)) (* 2 2) (* 3 1))
+(+ (+ 2 2 0) 4 3)
+(+ 4 4 3)
+11
+|#
+
+(define (f n)
+  (cond ((< n 3) n)
+        (else (f-iter 0 1 2 (- n 2)))))
+
+(define (f-iter a b c counter)
+  (display a) (display " ")
+  (display b) (display " ")
+  (display c) (display " ")
+  (display counter) (display " ")
+  (display "\n")
+  (if (= counter 0)
+    c
+    (f-iter b c (+ c (* 2 b) (* 3 a)) (- counter 1))))
+
+#|
+(f 6)
+(f-iter 0 1 2 4)
+(f-iter 1 2 4 3)
+(f-iter 2 4 11 2)
+(f-iter 4 11 25 1)
+(f-iter 11 25 59 0)
+59
+|#
+
+(f 6)
