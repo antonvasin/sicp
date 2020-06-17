f65f4059c62da75c2d257b361c6882f7d21378cc
Anton Vasin <antonvasin@me.com>
2020-06-17 22:03:39 +0300

    1.18

---
+++ chapter-1/1.18.rkt
@@ -0,0 +1,17 @@
+#lang sicp
+
+(define (double a) (+ a a))
+(define (halve a) (/ a 2))
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (iter-mult a b)
+  (iter-mult-impl a b 0))
+
+(define (iter-mult-impl a b n)
+  (cond ((= b 0) n)
+        ((even? a) (iter-mult-impl (halve a) (double b) n))
+        (else (iter-mult-impl a (- b 1) (+ n a)))))
+
+(iter-mult 7 5)
+(iter-mult 8 5)
